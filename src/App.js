2b80d3e7f6772e9ed9ea86572b852ed1a08f3a99
nav302004 <navv5803@gmail.com>
2024-02-04 22:01:01 +0530

    Shifting lapi

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Navbar from './components/Navbar';
 import { Routes, Route } from "react-router-dom"
 import Info from './components/pages/Info';
 import Project from './components/pages/Project';
+import FlyingImg from "./Assests/flyingone.png"
 
 function App() {
   return (
     <div className="App">
+      <img src={FlyingImg} alt="" />
       <Navbar />
         <Routes>
           <Route path='/' element={<Info/>} />
